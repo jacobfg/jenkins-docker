1e981ed4dbd76c20e543489fcb67d0b58690c249
Jacob Fleming-Gale <jacob@onamish.com>
2018-11-21 00:06:55 +1100

    Adding Global Variables

---
+++ files/usr/share/jenkins/ref/init.groovy.d/30_global_vars.groovy
@@ -0,0 +1,38 @@
+#!groovy
+
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+import groovy.json.JsonSlurper
+
+instance = Jenkins.getInstance()
+globalNodeProperties = instance.getGlobalNodeProperties()
+envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+
+println("--- Setting Global Variables")
+
+// skip if missing or parse error
+try {
+  def text = new File("/run/secrets/jenkins-global-vars").text
+  def json = new JsonSlurper().parseText(text)
+
+  envVars = null
+  if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+    newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+    globalNodeProperties.add(newEnvVarsNodeProperty)
+    envVars = newEnvVarsNodeProperty.getEnvVars()
+  } else {
+    envVars = envVarsNodePropertyList.get(0).getEnvVars()
+  }
+
+  json.each {
+    println("--- Adding ${it['name']}")
+    envVars.put(it["name"], it["value"])
+  }
+
+  instance.save()
+} catch (ex) {
+  println "ERROR: ${ex}"
+  println configXml.stripIndent()
+}
