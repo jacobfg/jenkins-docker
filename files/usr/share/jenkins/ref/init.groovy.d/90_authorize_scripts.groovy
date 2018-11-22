345772750429229da1d3bbcb2217c93ac9d72b8f
Jacob Fleming-Gale <jacob@onamish.com>
2018-11-22 15:24:25 +1100

    Full automated build and execution of approved seed job

--- files/usr/share/jenkins/ref/init.groovy.d/90_authorize_scripts.groovy
+++ files/usr/share/jenkins/ref/init.groovy.d/90_authorize_scripts.groovy
@@ -1,6 +1,35 @@
 #!groovy
 
 import org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval
+import jenkins.model.Jenkins
+
+def jobName = "SeedJob"
+
+seedJob = Jenkins.instance.getItem(jobName)
+
+while(true) {
+  if (seedJob) break
+  println("--- Waiting for Seed Job to be created")
+  sleep(1000)
+  // update status info
+  seedJob = Jenkins.instance.getItem(jobName)
+}
+
+println("--- Create Seed Job")
+seedJob.scheduleBuild(0, null)
+
+seedJob = Jenkins.instance.getItem(jobName)
+
+def running = (seedJob.lastBuild != null) ? seedJob.lastBuild.building : true
+// wait for job builds to complete
+while(true) {
+  if (!running) break
+  // println("--- Waiting for Seed Job to finish")
+  sleep(60000)
+  // update status info
+  seedJob = Jenkins.instance.getItem(jobName)
+  running = seedJob.lastBuild.building
+}
 
 ScriptApproval sa = ScriptApproval.get();
 
@@ -9,3 +38,11 @@ for (ScriptApproval.PendingScript pending : sa.getPendingScripts()) {
   println("--- Approve Pending Scripts")
   sa.approveScript(pending.getHash());
 }
+
+println("--- Clean Seed Job Build History")
+seedJob.getBuilds().each { it.delete() }
+seedJob.nextBuildNumber = 1
+seedJob.save()
+
+println("--- Rerunning Seed Job, Scripts Approved")
+seedJob.scheduleBuild(0, null)
