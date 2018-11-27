33a41b9e82532131694dda631d8dcb067af18145
Jacob Fleming-Gale <jacob@onamish.com>
2018-11-28 09:42:24 +1100

    Add git-parameter plugin Fix update plugin script

--- scripts/update_plugins.sh
+++ scripts/update_plugins.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eu
 
-USER=$(cat .jenkins-user.txt)
-PASS=$(cat .jenkins-pass.txt)
-JENKINS_HOST="${USER}:${PASS}@localhost:8081"
-curl -sSL "http://$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" \
+USER="admin"
+PASS=$(cat config/adminpw.txt)
+JENKINS_HOST="${USER}:${PASS}@localhost:8443"
+curl -ksSL "https://$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" \
   | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g' \
   | sed 's/ /:/' \
   | sort \
